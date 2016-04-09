6aecf5c04484652cca29136f8993742a7e4de61d
Henry Saniuk <henry@henrysaniuk.com>
2016-04-09 15:42:59 -0400

    Removed header from profile

--- dashboard/header.php
+++ dashboard/header.php
@@ -70,7 +70,6 @@
           <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
 
             <div class="menu_section">
-              <h3>General</h3>
               <ul class="nav side-menu">
                 <li><a href="index.php"><i class="fa fa-home"></i> Home </span></a></li>
                 <li><a><i class="fa fa-money"></i> PiggyBanks <span class="fa fa-chevron-down"></span></a>
--- dashboard/index.php
+++ dashboard/index.php
@@ -10,25 +10,6 @@
           <div class="row">
             <div class="col-md-12 col-sm-12 col-xs-12">
               <div class="x_panel">
-                <div class="x_title">
-                  <h2>User Report <small>Activity report</small></h2>
-                  <ul class="nav navbar-right panel_toolbox">
-                    <li><a href="#"><i class="fa fa-chevron-up"></i></a>
-                    </li>
-                    <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                      <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Settings 1</a>
-                        </li>
-                        <li><a href="#">Settings 2</a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li><a href="#"><i class="fa fa-close"></i></a>
-                    </li>
-                  </ul>
-                  <div class="clearfix"></div>
-                </div>
                 <div class="x_content">
 
                   <div class="col-md-3 col-sm-3 col-xs-12 profile_left">
