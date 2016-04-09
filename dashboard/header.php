764c8610f5795535deb13e9c2af219a4a6987add
Henry Saniuk <henry@henrysaniuk.com>
2016-04-09 15:53:16 -0400

    added table to profile page

--- dashboard/header.php
+++ dashboard/header.php
@@ -65,6 +65,7 @@
           <!-- /menu prile quick info -->
 
           <br />
+            <br />
 
           <!-- sidebar menu -->
           <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
--- dashboard/index.php
+++ dashboard/index.php
@@ -103,7 +103,7 @@
 
                       <li class="m-top-xs">
                         <i class="fa fa-external-link user-profile-icon"></i>
-                        <a href="http://henrysaniuk.com" target="_blank">www.henrysaniuk.com</a>
+                        <a href="http://henrysaniuk.com" target="_blank">henrysaniuk.com</a>
                       </li>
                     </ul>
 
@@ -131,75 +131,65 @@
                       <div id="myTabContent" class="tab-content">
                         <div role="tabpanel" class="tab-pane fade active in" id="tab_content1" aria-labelledby="home-tab">
 
-                          <!-- start recent activity -->
-                          <ul class="messages">
-                            <li>
-                              <img src="images/img.jpg" class="avatar" alt="Avatar">
-                              <div class="message_date">
-                                <h3 class="date text-info">24</h3>
-                                <p class="month">May</p>
-                              </div>
-                              <div class="message_wrapper">
-                                <h4 class="heading">Desmond Davison</h4>
-                                <blockquote class="message">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth.</blockquote>
-                                <br />
-                                <p class="url">
-                                  <span class="fs1 text-info" aria-hidden="true" data-icon=""></span>
-                                  <a href="#"><i class="fa fa-paperclip"></i> User Acceptance Test.doc </a>
-                                </p>
-                              </div>
-                            </li>
-                            <li>
-                              <img src="images/img.jpg" class="avatar" alt="Avatar">
-                              <div class="message_date">
-                                <h3 class="date text-error">21</h3>
-                                <p class="month">May</p>
-                              </div>
-                              <div class="message_wrapper">
-                                <h4 class="heading">Brian Michaels</h4>
-                                <blockquote class="message">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth.</blockquote>
-                                <br />
-                                <p class="url">
-                                  <span class="fs1" aria-hidden="true" data-icon=""></span>
-                                  <a href="#" data-original-title="">Download</a>
-                                </p>
-                              </div>
-                            </li>
-                            <li>
-                              <img src="images/img.jpg" class="avatar" alt="Avatar">
-                              <div class="message_date">
-                                <h3 class="date text-info">24</h3>
-                                <p class="month">May</p>
-                              </div>
-                              <div class="message_wrapper">
-                                <h4 class="heading">Desmond Davison</h4>
-                                <blockquote class="message">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth.</blockquote>
-                                <br />
-                                <p class="url">
-                                  <span class="fs1 text-info" aria-hidden="true" data-icon=""></span>
-                                  <a href="#"><i class="fa fa-paperclip"></i> User Acceptance Test.doc </a>
-                                </p>
-                              </div>
-                            </li>
-                            <li>
-                              <img src="images/img.jpg" class="avatar" alt="Avatar">
-                              <div class="message_date">
-                                <h3 class="date text-error">21</h3>
-                                <p class="month">May</p>
-                              </div>
-                              <div class="message_wrapper">
-                                <h4 class="heading">Brian Michaels</h4>
-                                <blockquote class="message">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth.</blockquote>
-                                <br />
-                                <p class="url">
-                                  <span class="fs1" aria-hidden="true" data-icon=""></span>
-                                  <a href="#" data-original-title="">Download</a>
-                                </p>
-                              </div>
-                            </li>
-
-                          </ul>
-                          <!-- end recent activity -->
+                           <!-- start user projects -->
+                          <table class="data table table-striped no-margin">
+                            <thead>
+                              <tr>
+                                <th>#</th>
+                                <th>Project Name</th>
+                                <th>Client Company</th>
+                                <th class="hidden-phone">Hours Spent</th>
+                                <th>Contribution</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>1</td>
+                                <td>New Company Takeover Review</td>
+                                <td>Deveint Inc</td>
+                                <td class="hidden-phone">18</td>
+                                <td class="vertical-align-mid">
+                                  <div class="progress">
+                                    <div class="progress-bar progress-bar-success" data-transitiongoal="35"></div>
+                                  </div>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>2</td>
+                                <td>New Partner Contracts Consultanci</td>
+                                <td>Deveint Inc</td>
+                                <td class="hidden-phone">13</td>
+                                <td class="vertical-align-mid">
+                                  <div class="progress">
+                                    <div class="progress-bar progress-bar-danger" data-transitiongoal="15"></div>
+                                  </div>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>3</td>
+                                <td>Partners and Inverstors report</td>
+                                <td>Deveint Inc</td>
+                                <td class="hidden-phone">30</td>
+                                <td class="vertical-align-mid">
+                                  <div class="progress">
+                                    <div class="progress-bar progress-bar-success" data-transitiongoal="45"></div>
+                                  </div>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>4</td>
+                                <td>New Company Takeover Review</td>
+                                <td>Deveint Inc</td>
+                                <td class="hidden-phone">28</td>
+                                <td class="vertical-align-mid">
+                                  <div class="progress">
+                                    <div class="progress-bar progress-bar-success" data-transitiongoal="75"></div>
+                                  </div>
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                          <!-- end user projects -->
 
                         </div>
                         <div role="tabpanel" class="tab-pane fade" id="tab_content2" aria-labelledby="profile-tab">
@@ -277,8 +267,18 @@
             </div>
           </div>
         </div>
-          
-                </div>
+
+        <!-- footer content -->
+        <footer>
+          <div class="copyright-info">
+            <p class="pull-right">Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>  
+            </p>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+
+      </div>
       <!-- /page content -->
     </div>
 
