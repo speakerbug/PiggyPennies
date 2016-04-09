3aa154fbf5a4458f654b2a42ec7ea915ced4e949
Sneha Vaswani <svaswani08@yahoo.com>
2016-04-09 17:57:26 -0400

    Added friends page

---
+++ dashboard/friends.php
@@ -0,0 +1,94 @@
+<?php include("header.php"); ?>
+<style>
+    .sneha-h1 {
+      color: #E64787;
+    }
+
+    .spacing-right {
+      margin-right: 20px;
+      margin-bottom: 20px;
+      margin-left: 10px;
+    }
+    </style>
+
+            <div class="right_col" role="main">
+                <div class="">
+                    <div class="page-title">
+                        <div class="title_left">
+                            <h3>Friends</h3>
+                        </div>
+
+                        <div class="title_right">
+                            <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Search for...">
+                                    <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">Go!</button>
+                        </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+
+                    <div class="row">
+
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <div class="x_panel" style="height:600px;">
+                                <div class="x_title">
+                                    <h2>Pennies Saved</h2>
+                                    <ul class="nav navbar-right panel_toolbox">
+                                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                                        </li>
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                                            <ul class="dropdown-menu" role="menu">
+                                                <li><a href="#">Settings 1</a>
+                                                </li>
+                                                <li><a href="#">Settings 2</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li><a class="close-link"><i class="fa fa-close"></i></a>
+                                        </li>
+                                    </ul>
+                                    <div class="clearfix"></div>
+                                </div>
+
+                                <div class="row">
+                                    <img src="https://s3-us-west-2.amazonaws.com/piggypennies/images/sneha.png" alt="..." class="pull-left img-circle spacing-right ">
+                                    <h2 class="sneha-h1">Sneha Vaswani</h2><br>
+                                    <h2>Has saved $32.75</h2><br>
+                                  </div>
+
+                                  <div class="row">
+                                    <img src="https://s3-us-west-2.amazonaws.com/piggypennies/images/stuart.png" alt="..." class="pull-left img-circle spacing-right">
+                                    <h2 class="sneha-h1">Stuart Olivera</h2><br>
+                                    <h2>Has saved $2.04</h2><br>
+                                  </div>
+
+                                  <div class="row">
+                                    <img src="https://s3-us-west-2.amazonaws.com/piggypennies/images/henry.jpg" alt="..." class="pull-left img-circle spacing-right">
+                                    <h2 class="sneha-h1">John Smith</h2><br>
+                                    <h2>Has saved $47.82</h2>
+                                  </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- footer content -->
+                <footer>
+                    <div class="">
+                        <p class="pull-right">Gentelella Alela! a Bootstrap 3 template by <a>Kimlabs</a>. |
+                            <span class="lead"> <i class="fa fa-paw"></i> Gentelella Alela!</span>
+                        </p>
+                    </div>
+                    <div class="clearfix"></div>
+                </footer>
+                <!-- /footer content -->
+
+            </div>
+            <!-- /page content -->
+
+<?php include("footer.php"); ?>
