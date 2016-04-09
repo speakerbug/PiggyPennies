bdba80e50cba6f1a729e55c79590b126c2f1eeab
Stuart Olivera <stuart@stuartolivera.com>
2016-04-09 15:46:29 -0400

    Fixed Sparkpost inbound handling

--- hooks/sparkpost.php
+++ hooks/sparkpost.php
@@ -10,11 +10,31 @@ function print_line($message) {
   echo $message . "<br>";
 }
 
+function transfer_money($message) {
+  // do the Capital One transfer API here
+  return true;
+}
+
+function send_response($message) {
+  $to      = $message["msg_from"];
+  $subject = 'Re: ' . $message["content"]["subject"];
+  $content = "Success! We've made your transfer.";
+  $headers = 'From: hello@piggy.aws-cloud.oweb.co' . "\r\n" .
+      'Reply-To: hello@piggy.aws-cloud.oweb.co' . "\r\n" .
+      'X-Mailer: PHP/' . phpversion();
+  mail($to, $subject, $content, $headers);
+}
+
 foreach ($messages as $message) {
-  $message = $message["msys"]["relay_event"];
-  print_line("New message from: " . $message["msg_from"]);
+  $message = $message["msys"]["relay_message"];
+  print_line("New message from: " . $message["friendly_from"]);
   print_line("To: " . $message["rcpt_to"]);
-  mail($message["msg_from"], "Received", "hello");
+
+  if (transfer_money($message)) {
+    send_response($message);
+  } else {
+    echo "Failed";
+  }
 }
 
 ?>
