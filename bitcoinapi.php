166319acd11039cebcc47409a17ea591427b8b71
Henry Saniuk <henry@henrysaniuk.com>
2016-04-09 13:42:00 -0400

    basic bitcoin get value

---
+++ bitcoinapi.php
@@ -0,0 +1,6 @@
+<?php
+$bitcoin_price = file_get_contents('https://api.coindesk.com/v1/bpi/currentprice.json');
+$price = json_decode($bitcoin_price, true)
+$usd = $price['bpi']['rate_float'];
+echo $usd;
+?>
