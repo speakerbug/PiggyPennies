d0ad5ea2ee9052b0558bb900362df861d97ebefe
Henry Saniuk <henry@henrysaniuk.com>
2016-04-09 16:27:58 -0400

    decreased spacing in nav bar

--- dashboard/header.php
+++ dashboard/header.php
@@ -68,7 +68,7 @@
 
           <!-- sidebar menu -->
           <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-<br>
+
             <div class="menu_section">
               <ul class="nav side-menu">
                 <li><a href="index.php"><i class="fa fa-home"></i> Home </span></a></li>
