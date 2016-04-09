b5380d49d9fc1448589b175ca76db792095c32a4
Henry Saniuk <henry@henrysaniuk.com>
2016-04-09 16:26:25 -0400

    Removed footer

--- dashboard/index.php
+++ dashboard/index.php
@@ -268,16 +268,6 @@
           </div>
         </div>
 
-        <!-- footer content -->
-        <footer>
-          <div class="copyright-info">
-            <p class="pull-right">Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>  
-            </p>
-          </div>
-          <div class="clearfix"></div>
-        </footer>
-        <!-- /footer content -->
-
       </div>
       <!-- /page content -->
     </div>
