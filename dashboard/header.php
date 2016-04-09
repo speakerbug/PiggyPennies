bc41360e778f42d499a9ec828cd94bacf1f53f94
Henry Saniuk <henry@henrysaniuk.com>
2016-04-09 14:57:24 -0400

    Changed header

--- dashboard/header.php
+++ dashboard/header.php
@@ -121,7 +121,7 @@
             <ul class="nav navbar-nav navbar-right">
               <li class="">
                 <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                  <img src="images/img.jpg" alt="">John Doe
+                  <img src="https://s3-us-west-2.amazonaws.com/piggypennies/images/henry.jpg" alt="">Henry
                   <span class=" fa fa-angle-down"></span>
                 </a>
                 <ul class="dropdown-menu dropdown-usermenu animated fadeInDown pull-right">
