5d1fd73076d208410488c589f56868c675793847
Stuart Olivera <stuart@stuartolivera.com>
2016-04-09 15:04:36 -0400

    Enabled basic sparkpost hook

--- hooks/sparkpost.php
+++ hooks/sparkpost.php
@@ -1,1 +1,20 @@
-<?php echo "hi"; ?>
+<?php
+
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
+$inputJSON = file_get_contents('php://input');
+$messages = json_decode( $inputJSON, TRUE );
+
+function print_line($message) {
+  echo $message . "<br>";
+}
+
+foreach ($messages as $message) {
+  $message = $message["msys"]["relay_event"];
+  print_line("New message from: " . $message["msg_from"]);
+  print_line("To: " . $message["rcpt_to"]);
+  mail($message["msg_from"], "Received", "hello");
+}
+
+?>
